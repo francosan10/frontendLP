ae81980c23ab7addb56f9cf618855bb9d5434412
francosan10 <56896216+francosan10@users.noreply.github.com>
2023-06-13 23:40:55 -0300

    agregamos javascript
    boton claro y oscuro, alerta de bienvenida y boton copyright con alerta.

---
+++ scripts/script.js
@@ -0,0 +1,27 @@
+function oscuro() {
+    document.body.style.background = '#192229';
+    document.getElementById('h3').style.color = 'white';
+    document.getElementById('ul').style.color = 'white';
+    document.getElementById('ul2').style.color = 'white';
+    document.getElementById('ol').style.color = 'white';
+    document.getElementById('t1').style.color = 'white';
+    document.getElementById('t2').style.color = 'white';
+    document.getElementById('t3').style.color = 'white';
+    document.getElementById('t4').style.color = 'white';
+}
+
+function claro() {
+    document.body.style.background = 'url(../imagenes/fondoop.webp)';
+    document.getElementById('h3').style.color = '#111111';
+    document.getElementById('ul').style.color = '#111111';
+    document.getElementById('ul2').style.color = '#111111';
+    document.getElementById('ol').style.color = '#111111';
+    document.getElementById('t1').style.color = '#111111';
+    document.getElementById('t2').style.color = '#111111';
+    document.getElementById('t3').style.color = '#111111';
+    document.getElementById('t4').style.color = '#111111';
+}
+
+function autor() {
+    alert('página creada por Franco Sánchez');
+}
